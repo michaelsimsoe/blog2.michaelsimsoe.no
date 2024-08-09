c7743deec7e5c9b599cd930e763a570f5b9b8bf2
Michael Krøyserth-Simsø <michaelkr@microsoft.com>
2024-08-09 22:15:06 +0200

    feat: more view transitions

--- src/app/main.ts
+++ src/app/main.ts
@@ -17,14 +17,14 @@
 //         console.error('Error fetching or displaying posts:', error);
 //     }
 // });
-console.log("main.ts loaded");
+// console.log("main.ts loaded");
 
-document.addEventListener("DOMContentLoaded", async () => {
-  window.addEventListener("pageswap", (event) => {
-    console.log("Page swap event:", event);
-  });
+// document.addEventListener("DOMContentLoaded", async () => {
+//   window.addEventListener("pageswap", (event) => {
+//     console.log("Page swap event:", event);
+//   });
 
-  window.addEventListener("pagereveal", (event) => {
-    console.log("Page reveal event:", event);
-  });
-});
+//   window.addEventListener("pagereveal", (event) => {
+//     console.log("Page reveal event:", event);
+//   });
+// });
